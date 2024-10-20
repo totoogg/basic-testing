0bdc8643619d845152faed4495a699c50838629f
totoogg <goncharov.vova2014@gamil.com>
2024-10-20 15:56:28 +0300

    feat: add snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,27 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const result = {
+      next: {
+        next: {
+          next: {
+            next: null,
+            value: null,
+          },
+          value: 1,
+        },
+        value: 1,
+      },
+      value: 1,
+    };
+    expect(generateLinkedList([1, 1, 1])).toStrictEqual(result);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 1, 1])).toMatchSnapshot();
   });
 });
